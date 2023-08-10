b43e1c3cb64fcaf14e22d04cc83d5865b3329011
Chinthani <chinthanichandeepa99@gmail.com>
2023-08-10 13:53:58 +0530

    Updated book register form

--- src/layouts/full/sidebar/MenuItems.js
+++ src/layouts/full/sidebar/MenuItems.js
@@ -1,5 +1,5 @@
 import {
-  IconAperture, IconCopy, IconLayoutDashboard, IconLogin, IconMoodHappy, IconTypography, IconUserPlus
+  IconLayoutDashboard, IconLogin, IconBook2, IconUserPlus
 } from '@tabler/icons';
 
 import { uniqueId } from 'lodash';
@@ -16,22 +16,14 @@ const Menuitems = [
     icon: IconLayoutDashboard,
     href: '/dashboard',
   },
-  {
-    navlabel: true,
-    subheader: 'Utilities',
-  },
+
   {
     id: uniqueId(),
-    title: 'Typography',
-    icon: IconTypography,
-    href: '/ui/typography',
+    title: 'Book Register',
+    icon: IconBook2,
+    href: '/registerbook',
   },
-  {
-    id: uniqueId(),
-    title: 'Shadow',
-    icon: IconCopy,
-    href: '/ui/shadow',
-  },
+  
   {
     navlabel: true,
     subheader: 'Auth',
@@ -48,22 +40,7 @@ const Menuitems = [
     icon: IconUserPlus,
     href: '/auth/register',
   },
-  {
-    navlabel: true,
-    subheader: 'Extra',
-  },
-  {
-    id: uniqueId(),
-    title: 'Icons',
-    icon: IconMoodHappy,
-    href: '/icons',
-  },
-  {
-    id: uniqueId(),
-    title: 'Sample Page',
-    icon: IconAperture,
-    href: '/sample-page',
-  },
+  
 ];
 
 export default Menuitems;
