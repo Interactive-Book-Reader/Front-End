7a6d91e53c340c0eee9222b81cb26cf40c863508
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-08-24 14:42:20 +0530

    Changed the cover page

--- src/components/CoverPage/CoverPage.js
+++ src/components/CoverPage/CoverPage.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { CardContent, Typography, Grid } from '@mui/material';
+import { Typography, Grid } from '@mui/material';
 
-import { Stack } from '@mui/system';
-import BlankCard from '../shared/BlankCard';
-
-
-
-const CoverPage = ({title,author,photo,price}) => {
+const CoverPage = ({ title, author, photo, price }) => {
   return (
     <Grid container spacing={4}>
-        <Grid item sm={12} md={4} lg={3}>
-          <BlankCard>
-            <Typography component={Link} to="/mybook">
-              <img src={photo} alt="img" width="100%" />
-            </Typography>
-            <CardContent sx={{ p: 3, pt: 2 }}>
-              <Typography variant="h6">{title}</Typography>
-              <Typography variant="h7">{author}</Typography>
-              <Stack direction="row" alignItems="center" justifyContent="space-between" mt={1}>
-                <Stack direction="row" alignItems="center">
-                  <Typography variant="h6">LKR{price}</Typography>
-                </Stack>
-              </Stack>
-            </CardContent>
-          </BlankCard>
-        </Grid>
-    
+      <Grid item sm={12} md={4} lg={3}>
+        <div style={{ height: '400px', width: '300px' }}>
+          <Link to="/mybook">
+            <img
+              src={photo}
+              alt="img"
+              style={{
+                border: '3px solid black', // Adds a black border
+                borderRadius: '40px', // Adds corner radius
+                width: '300px', // Sets width to 300px
+                height: '300px', // Sets height to 300px
+              }}
+            />
+          </Link>
+          <div style={{ height: '100px', width: '300px' }}>
+            <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+              <Typography variant="h5" style={{ color: 'blue', marginBottom: '8px' }}>
+                {title}
+              </Typography>
+              <Typography variant="h6" style={{ marginBottom: '4px' }}>
+                {author}
+              </Typography>
+              <Typography variant="h6" style={{ marginBottom: '4px', fontWeight: 'bold' }}>
+                LKR{price}
+              </Typography>
+            </div>
+          </div>
+        </div>
+      </Grid>
     </Grid>
   );
 };
