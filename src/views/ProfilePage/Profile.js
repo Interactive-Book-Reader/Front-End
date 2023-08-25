463a2413f268be1a3f64488c922ca1598a0ec0e3
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-08-25 12:37:57 +0530

    Profile Page is partially created

--- src/views/ProfilePage/Profile.js
+++ src/views/ProfilePage/Profile.js
@@ -1,39 +1,54 @@
-import { Typography,TextField } from '@mui/material';
+import { Typography } from '@mui/material';
 import React from 'react';
+import TextBox from 'src/components/TextBox/TextBox';
+import DateField from 'src/components/DateField/DateField';
+import DropDownList from 'src/components/DropDownList/DropDownList';
 
 const Profile = () => {
   return (
-    <div
-          className="mx-8 mx-8"
-          style={{
-              width: '800px',
-              height: '500px',
-              backgroundColor: '#F2F2F2',
-              border: '2px solid black',
-              borderRadius: '30px',
+    <>
+      <Typography
+        style={{
+          fontSize: '30px',
+          fontWeight: 'bold',
+          color: 'black',
+          textAlign: 'justify',
+          marginLeft: '40px',
+          marginTop: '10px',
+        }}
+      >
+        General information
+      </Typography>
+      <div style={{ display: 'flex', paddingTop: '10px' }}>
+        <TextBox inputText="First Name" label="Enter the first name:" width="350px" />
+        <TextBox inputText="Last Name" label="Enter the last name:" width="350px" />
+      </div>
+      <div style={{ display: 'flex', paddingTop: '10px' }}>
+        <TextBox inputText="Email Address" label="name@gmail.com" width="350px" />
+        <TextBox inputText="Phone" label="+94 77 004 2188" width="350px" />
+      </div>
+      <div style={{ width: '350px', paddingTop: '10px', display: 'flex', alignItems: 'center' }}>
+        <Typography
+          variant="h7"
+          component="div"
+          sx={{
+            flexGrow: 1,
+            fontSize: '20px',
+            color: 'black',
+            textAlign: 'justify',
+            marginLeft: '10px',
+            marginTop: '10px',
           }}
-      ><div style={{
-         width: '50%',
-      }}>
-        <Typography style={{
-              fontSize: '30px',
-              fontWeight: 'bold',
-              color: 'black',
-              textAlign: 'justify',
-              marginLeft: '40px',
-              marginTop: '10px',
-          }}>General information</Typography>
-           <Typography variant="h7" component="div" sx={{ flexGrow: 1 }} style={{
-                fontSize: '20px',
-                color: 'black',
-                textAlign: 'justify',
-                marginLeft: '10px',
-                marginTop: '10px',
-           }}>First Name</Typography>
-           <TextField id="outlined-basic" label="First Name" variant="outlined" />
-      </div>          
-    </div>
-    
+        >
+          Date of Birth
+        </Typography>
+        <DateField text="Date of Birth" width="350px" />
+      </div>
+      <div style={{alignItems:'center'}}>
+
+      </div>
+      <DropDownList />
+    </>
   );
 };
 
