c92e0be88a8a0d1753178f91948edbc9c55cfe17
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-08-25 12:13:30 +0530

    Date picker is created

---
+++ src/components/DateField/DateField.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+const DateField = ({ text, width }) => {
+  return (
+    <div style={{ width: width, marginRight: '20px' }}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={['DatePicker']}>
+          <DatePicker
+            label={text}
+            inputProps={{
+              style: { width: '350px' }, // Set the width here
+            }}
+          />
+        </DemoContainer>
+      </LocalizationProvider>
+    </div>
+  );
+};
+
+export default DateField;
