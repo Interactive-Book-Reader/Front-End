7537769c9d1b3bc0a4dcefefc5013dd92c7929b4
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-08-30 23:35:14 +0530

    Profile image can be edited as well

--- src/components/DateField/YearPicker.js
+++ src/components/DateField/YearPicker.js
@@ -2,28 +2,17 @@ import React from 'react';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import TextField from '@mui/material/TextField';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const YearPicker= ({ label,onInputChange}) => {
-  
+  const printDate = (date) => {
+    onInputChange(date.format('YYYY'));
+  };
   return (
     <div style={{ marginRight: '20px' }}>
-      
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DemoContainer components={['DatePicker']}>
-        <TextField
-                  required
-                  fullWidth
-                  id="date"
-                  label="Date of birth"
-                  type="date"
-                  name="birthday"
-                  autoComplete="family-name"
-                  onChange={(event) => onInputChange(event.target.value)}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                />
+          <DatePicker label={label} views={['year']} onChange={printDate}/>
         </DemoContainer>
       </LocalizationProvider>
     </div>
