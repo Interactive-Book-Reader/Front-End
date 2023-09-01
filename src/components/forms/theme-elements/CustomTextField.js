2c028d39cb71600d6ea9a31362206b6d2f3c2c8b
Amrithshagar <amrithshagar.20@cse.mrt.ac.lk>
2023-09-01 17:43:30 +0530

    Improved UI of book register page

--- src/components/forms/theme-elements/CustomTextField.js
+++ src/components/forms/theme-elements/CustomTextField.js
@@ -5,15 +5,26 @@ import { TextField } from '@mui/material';
 const CustomTextField = styled((props) => <TextField {...props} />)(({ theme }) => ({
   '& .MuiOutlinedInput-input::-webkit-input-placeholder': {
     color: theme.palette.text.secondary,
-    opacity: '0.',
+    opacity: '0',
   },
   '& .MuiOutlinedInput-input.Mui-disabled::-webkit-input-placeholder': {
     color: theme.palette.text.secondary,
     opacity: '1',
   },
-  '& .Mui-disabled .MuiOutlinedInput-notchedOutline': {
-    borderColor: theme.palette.grey[200],
+  '& .MuiOutlinedInput-root': {
+    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+    transition: 'box-shadow 0.3s ease, padding 0.3s ease', // Add transitions for box shadow and padding
+    width: '100%',
+    padding: '10px', // Initial padding
+    height: 'auto', // Initial height
+  },
+  '&:hover .MuiOutlinedInput-root': {
+    boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.2)',
+    padding: '20px', // Expanded padding on hover
+    height: 'auto', // Expanded height on hover
   },
 }));
 
-export default CustomTextField;
+export default function CustomTextFieldExpandedOnHover(props) {
+  return <CustomTextField {...props} />;
+}
