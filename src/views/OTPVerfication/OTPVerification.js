989f2fe609ad7b9cda05ac548c8d520de8247c55
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-09-03 21:32:18 +0530

    Create OTP Verification Page

--- src/views/OTPVerfication/OTPVerification.js
+++ src/views/OTPVerfication/OTPVerification.js
@@ -3,6 +3,8 @@ import PageContainer from 'src/components/container/PageContainer';
 import OtpInput from 'src/components/OTPInput/OtpInput';
 
 const OTPVerification = () => {
+    const url = new URL(window.location.href);
+    const id = url.searchParams.get('id');
   const [otp, setOtp] = useState('');
 
   // Callback function to handle OTP changes
@@ -10,14 +12,34 @@ const OTPVerification = () => {
     setOtp(value);
     // You can add logic here to verify the OTP or perform other actions.
   };
+
+  const handleSubmit = async() => {
+    const response = await fetch('http://localhost:3001/api/publisher/verifyOTP', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({otp: otp, publisherId: id}),  
+    });
+    const responseData = await response.json();
+    if (responseData.message==='OTP is verified successfully.'){
+        console.log(responseData);
+        window.location.href = `/auth/login`;
+    }else{
+        console.log(responseData);
+    }
+    };
+
+
   return (
     <PageContainer title="OTP Verification Page" description="this is OTP Verification Page">
       <div>
         <div className="App">
           <h1>OTP Verification</h1>
           <OtpInput numInputs={6} onChange={handleOtpChange} />
-          <p>Entered OTP: {otp}</p>
+          <p>Entered OTP: {id}</p>
         </div>
+        <button onClick={handleSubmit}>Submit</button>
       </div>
     </PageContainer>
   );
