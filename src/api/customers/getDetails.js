ec43ec79f9d9d3174181deecd09cf464f07cd68a
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-09-12 16:01:28 +0530

    change the getDetails api

--- src/api/customers/getDetails.js
+++ src/api/customers/getDetails.js
@@ -1,7 +1,13 @@
 import baseURL from 'src/config/config';
 
-const getDetails = async () => {
-  const response = await fetch(`${baseURL}/api/read_books/getdetails`);
+const getDetails = async (id) => {
+  const response = await fetch(`${baseURL}/api/read_books/getdetails`,{
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({publisher_id: id}),
+  });
   const data = await response.json();
   return data;
 };
