767e340d9381af88438d277d772b8860e08ebc6b
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-09-13 22:49:29 +0530

    add some changes

--- src/components/DropDownList/DropDownList.js
+++ src/components/DropDownList/DropDownList.js
@@ -5,10 +5,10 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-const DropDownList = ({ label,items,onchange}) => {
+const DropDownList = ({ label, items, onchange }) => {
   const handleChange = (event) => {
     onchange(event.target.value); // Update the selectedValue state when the value changes
-  };  
+  };
   return (
     <Box sx={{ minWidth: 120 }}>
       <FormControl fullWidth>
@@ -17,18 +17,21 @@ const DropDownList = ({ label,items,onchange}) => {
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           label="Age"
-          onChange={handleChange} // Attach the handleChange event handler to the onChange event of Select
+          onChange={handleChange}
           sx={{
             '&:hover': {
-              fontSize: '16px', // Increase the font size
+              transform: 'scale(1.01)', // Increase the size on hover
+              transition: 'all 0.5s ease-in-out', // Add a smooth transition effect
+              padding: '3px',
+              boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.4)', // Add a shadow effect on hover
             },
           }}
         >
           {items.map((item) => (
-          <MenuItem key={item} value={item}>
-            {item}
-          </MenuItem>
-        ))}
+            <MenuItem key={item} value={item}>
+              {item}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
     </Box>
