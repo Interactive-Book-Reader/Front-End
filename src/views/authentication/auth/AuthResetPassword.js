7404e240aa6953709f235a87a37e4c9cbbf99f34
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-10-07 15:23:11 +0530

    Deplotment test

--- src/views/authentication/auth/AuthResetPassword.js
+++ src/views/authentication/auth/AuthResetPassword.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import CustomTextField from '../../../components/forms/theme-elements/CustomTextField';
 import resetPasswordFunction from 'src/api/auth/resetpassword';
 
-
 const AuthResetPassword = ({ title, subtitle, subtext, id, token }) => {
   const [error, setError] = useState('');
   const [password, setPassword] = useState('');
@@ -18,32 +17,15 @@ const AuthResetPassword = ({ title, subtitle, subtext, id, token }) => {
       token: token,
     };
 
-    // Regular expressions for symbol, number, and letter checks
-    const symbolRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/;
-    const numberRegex = /[0-9]/;
-    const letterRegex = /[a-zA-Z]/;
-
-    // Check if the password meets the criteria
-    const hasSymbol = symbolRegex.test(password);
-    const hasNumber = numberRegex.test(password);
-    const hasLetter = letterRegex.test(password);
-
-    // Check if all criteria are met
-    if (hasSymbol && hasNumber && hasLetter) {
-      try {
-        const response = await resetPasswordFunction(data);
-        if (response.message !== 'Password is updated successfully.') {
-          setError(response.message);
-        }
-        else{
-          setError(response.message);
-        }
-      } catch (error) {
-        setError(error.message);
+    try {
+      const response = await resetPasswordFunction(data);
+      if (response.message !== 'Password is updated successfully.') {
+        setError(response.message);
+      } else {
+        setError(response.message);
       }
-    } else {
-      setError('Password should contain at least one symbol, number, and letter');
-      // Handle invalid password here (e.g., show an error message)
+    } catch (error) {
+      setError(error.message);
     }
   };
 
